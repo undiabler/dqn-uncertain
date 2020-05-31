3f56bcb80c4b6f39a5cf1473979dae9bc4cf1530
undiabler <undiabler@gmail.com>
2020-05-31 14:05:30 +0200

    dueling architecture

--- simple.py
+++ simple.py
@@ -5,7 +5,7 @@ import numpy as np
 from collections import deque
 import tensorflow as tf
 
-class DQNAgent:
+class QAgent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
@@ -16,17 +16,44 @@ class DQNAgent:
         self.model = self._build_model()
 
     def _build_model(self):
+
         # Neural Net for Deep-Q learning Model
+        # using SELU as main activation 
+        # following
+        #   https://arxiv.org/abs/1706.02515
+
         inpx = tf.keras.layers.Input(shape=(self.state_size))
         x = inpx
-        x = tf.keras.layers.Dense(128, kernel_initializer='lecun_normal', activation='selu')(x)
-        x = tf.keras.layers.Dense(128, kernel_initializer='lecun_normal', activation='selu')(x)
-        x = tf.keras.layers.Dense(128, kernel_initializer='lecun_normal', activation='selu')(x)
+        x = tf.keras.layers.Dense(128, kernel_initializer='lecun_normal', activation='selu', use_bias=False)(x)
+        x = tf.keras.layers.Dense(128, kernel_initializer='lecun_normal', activation='selu', use_bias=False)(x)
+        x = tf.keras.layers.Dense(128, kernel_initializer='lecun_normal', activation='selu', use_bias=False)(x)
         x = tf.keras.layers.GaussianDropout(0.1)(x)
-        x = tf.keras.layers.Dense(self.action_size, activation='linear')(x)
 
+
+        def deepDueling(x, layers=[64,64,64]):
+            layer = x
+            for neurons in layers:
+                layer = tf.keras.layers.Dense(neurons, kernel_initializer='lecun_normal', activation='selu')(layer)
+            return layer
+
+        # Dueling network architectire
+        # following 
+        #   https://medium.com/analytics-vidhya/introduction-to-dueling-double-deep-q-network-d3qn-8353a42f9e55
+        #   https://towardsdatascience.com/dueling-deep-q-networks-81ffab672751
+
+        actions = deepDueling(x)
+        actions = tf.keras.layers.Dense(self.action_size, activation='softmax')(actions)
+        
+        value = deepDueling(x)
+        value = tf.keras.layers.Dense(1, activation='linear')(value)
+
+        # dueling X
+        x = tf.keras.layers.Flatten()(actions*value)
+
+        # Compiled model, using Huber loss
         model = tf.keras.Model(inputs=inpx, outputs=x)
-        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate))
+        model.compile(loss=tf.keras.losses.Huber(), optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate))
+        model.summary()
         return model
 
     def remember(self, state, action, reward, next_state, done):
@@ -36,6 +63,8 @@ class DQNAgent:
         # if training and np.random.rand() <= self.epsilon:
             # return random.randrange(self.action_size)
         act_values = self.model(state, training=training)
+        # if not training:
+            # print(act_values[0])
         return np.argmax(act_values[0]) # returns action
 
     def replay(self, batch_size):
@@ -80,21 +109,21 @@ class DQNAgent:
 
 
 env_name = 'LunarLander-v2'
-EPISODES = 1000
+EPISODES = 100
 
 if __name__ == "__main__":
     env = gym.make(env_name)
     state_size = env.observation_space.shape[0]
     # print(env.action_space.sample())
     action_size = 4
-    agent = DQNAgent(state_size, action_size)
+    agent = QAgent(state_size, action_size)
     agent.load(f"./model-{env_name}.h5")
 
     done = False
     batch_size = 128
 
-    # train=False
-    train=True
+    train=False
+    # train=True
     if train:
         for e in range(EPISODES):
             state = env.reset()
@@ -116,13 +145,14 @@ if __name__ == "__main__":
                 print(f"episode: {e: 4d}/{EPISODES:4d}, steps: {time: 4d}, reward: {cumReward:+7.2f}, loss = {loss:.2f}")
         agent.save(f"./model-{env_name}.h5")
     else:
-        env = gym.wrappers.Monitor(env, './videos/dqn-' + str(time.time()) + '/')
+        # env = gym.wrappers.Monitor(env, './videos/dqn-' + str(time.time()) + '/')
         state = env.reset()
         state = np.reshape(state, [1, state_size])
         cumReward = 0
         for time in range(5000):
             env.render()
             action = agent.act(state, False)
+            # print(action)
             next_state, reward, done, _ = env.step(action)
             next_state = np.reshape(next_state, [1, state_size])
             cumReward += reward
